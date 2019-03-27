4c3ca54b0c1abfbaffc70e48f32f8194e870c386
Xander Guzman <xander.guzman@xanderguzman.com>
2019-03-26 21:46:08 -0600

    * remove premature file

--- pieces.go
+++ pieces.go
@@ -1,10 +1,12 @@
 package hive
 
-type HivePiece int
+import "github.com/theshadow/sge"
+
+type PieceType int
 type PieceColor int
 
 const (
-	QueenPiece HivePiece = iota
+	QueenPiece PieceType = iota
 	GrassHopperPiece
 	SoldierAntPiece
 	BeetlePiece
@@ -12,13 +14,41 @@ const (
 	MosquitoPiece
 	LadybugPiece
 	PillBugPiece
-
-	BlackPieceColor PieceColor = iota
-	WhitePieceColor
 )
 
+// Point3D
+type Point3D [3]float64
 
+func (p Point3D) X() float64 {
+	return p[0]
+}
 
+func (p Point3D) Y() float64 {
+	return p[1]
+}
+
+func (p Point3D) Z() float64 {
+	return p[2]
+}
+
+// Piece
 type Piece struct {
-	h *sge.Hexagon
+	// where on the board is this piece located
+	loc    Point3D
+	// the renderable object
+	obj    sge.GraphObject
+	// the hive piece
+	typ    PieceType
+}
+
+func NewPiece(loc Point3D, o sge.GraphObject, t PieceType) *Piece {
+	return &Piece{
+		loc: loc,
+		obj: o,
+		typ: t,
+	}
+}
+
+type Grid struct {
+	pieces []Piece
 }
